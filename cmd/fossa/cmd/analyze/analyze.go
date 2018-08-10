5207b1b6ab37c908eebbdddeae659a3a627206a4
Leo Zhang <leo@leozhang.me>
2018-08-09 19:59:12 -0700

    WIP 6

--- cmd/fossa/cmd/analyze/analyze.go
+++ cmd/fossa/cmd/analyze/analyze.go
@@ -5,11 +5,13 @@ import (
 
 	"github.com/urfave/cli"
 
+	"github.com/apex/log"
 	"github.com/fossas/fossa-cli/analyzers"
 	"github.com/fossas/fossa-cli/api/fossa"
+	"github.com/fossas/fossa-cli/cmd/fossa/display"
 	"github.com/fossas/fossa-cli/cmd/fossa/flags"
+	"github.com/fossas/fossa-cli/cmd/fossa/setup"
 	"github.com/fossas/fossa-cli/config"
-	"github.com/apex/log"
 	"github.com/fossas/fossa-cli/module"
 )
 
@@ -29,28 +31,28 @@ var Cmd = cli.Command{
 var _ cli.ActionFunc = Run
 
 func Run(ctx *cli.Context) error {
-	err := display.Init(ctx)
+	err := setup.Setup(ctx)
 	if err != nil {
-		log.Logger.Fatalf("Could not initialize: %s", err.Error())
+		log.Fatalf("Could not initialize: %s", err.Error())
 	}
 
 	modules, err := config.Modules()
 	if err != nil {
-		log.Logger.Fatalf("Could not parse modules: %s", err.Error())
+		log.Fatalf("Could not parse modules: %s", err.Error())
 	}
 	if len(modules) == 0 {
-		log.Logger.Fatal("No modules specified.")
+		log.Fatal("No modules specified.")
 	}
 
 	analyzed, err := Do(modules)
 	if err != nil {
-		log.Logger.Fatalf("Could not analyze modules: %s", err.Error())
+		log.Fatalf("Could not analyze modules: %s", err.Error())
 		return err
 	}
 
 	normalized, err := fossa.Normalize(analyzed)
 	if err != nil {
-		log.Logger.Fatalf("Could not normalize output: %s", err.Error())
+		log.Fatalf("Could not normalize output: %s", err.Error())
 		return err
 	}
 
@@ -59,10 +61,10 @@ func Run(ctx *cli.Context) error {
 			output, err := display.TemplateFile(tmplFile, normalized)
 			fmt.Println(output)
 			if err != nil {
-				log.Logger.Fatalf("Could not parse template data: %s", err.Error())
+				log.Fatalf("Could not parse template data: %s", err.Error())
 			}
 		} else {
-			log.PrintJSON(normalized)
+			display.JSON(normalized)
 		}
 
 		return nil
@@ -77,19 +79,19 @@ func Do(modules []module.Module) (analyzed []module.Module, err error) {
 		display.InProgress(fmt.Sprintf("Analyzing module (%d/%d): %s", i+1, len(modules), m.Name))
 		analyzer, err := analyzers.New(m)
 		if err != nil {
-			log.Logger.Warningf("Could not load analyzer: %s", err.Error())
+			log.Warnf("Could not load analyzer: %s", err.Error())
 			continue
 		}
 		built, err := analyzer.IsBuilt()
 		if err != nil {
-			log.Logger.Warningf("Could not determine whether module is built: %s", err.Error())
+			log.Warnf("Could not determine whether module is built: %s", err.Error())
 		}
 		if !built {
-			log.Logger.Warningf("Module does not appear to be built")
+			log.Warnf("Module does not appear to be built")
 		}
 		deps, err := analyzer.Analyze()
 		if err != nil {
-			log.Logger.Fatalf("Could not analyze: %s", err.Error())
+			log.Fatalf("Could not analyze: %s", err.Error())
 		}
 		m.Imports = deps.Direct
 		m.Deps = deps.Transitive
@@ -101,14 +103,13 @@ func Do(modules []module.Module) (analyzed []module.Module, err error) {
 }
 
 func uploadAnalysis(normalized []fossa.SourceUnit) error {
-	fossa.MustInit(config.Endpoint(), config.APIKey())
 	display.InProgress("Uploading analysis...")
 	locator, err := fossa.Upload(config.Fetcher(), config.Project(), config.Revision(), config.Title(), config.Branch(), normalized)
 	display.ClearProgress()
 	if err != nil {
-		log.Logger.Fatalf("Error during upload: %s", err.Error())
+		log.Fatalf("Error during upload: %s", err.Error())
 		return err
 	}
-	log.Printf(display.ReportURL(locator))
+	fmt.Printf(display.ReportURL(locator))
 	return nil
 }
